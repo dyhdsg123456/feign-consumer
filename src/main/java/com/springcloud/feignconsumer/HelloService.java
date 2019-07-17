caf6a7bb3e35d5db852e8c1d63d40e04b1c8742d
duyuanhao <13775516233@163.com>
2019-07-17 16:43:27 +0800

    feign

---
+++ src/main/java/com/springcloud/feignconsumer/HelloService.java
@@ -0,0 +1,26 @@
+package com.springcloud.feignconsumer;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Author: dyh
+ * Date:   2019/7/17
+ * Description:
+ */
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String hello();
+
+    @RequestMapping("hello1")
+     String hello(@RequestParam("name") String name);
+
+    @RequestMapping("hello2")
+     User hello(@RequestHeader("name") String name, @RequestHeader("age") Integer age);
+
+    @RequestMapping(value = "hello3",method = RequestMethod.POST)
+    String hello(@RequestBody User user);
+
+}
